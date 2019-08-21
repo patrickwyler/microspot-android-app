555bb0ea935b7b9e82f72e7e253ff32c39d6ab6b
Patrick Wyler <patrick.wyler@interdiscount.ch>
2019-08-21 20:58:26 +0200

    on click go to product page

--- app/src/main/java/ch/wyler/microspot/fragments/SearchFragment.kt
+++ app/src/main/java/ch/wyler/microspot/fragments/SearchFragment.kt
@@ -1,6 +1,8 @@
 package ch.wyler.microspot.fragments
 
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -11,7 +13,7 @@ import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import ch.sik.teko.home.networking.RestApi
 import ch.wyler.microspot.R
-import ch.wyler.microspot.adapter.MatchListAdapter
+import ch.wyler.microspot.adapter.ProductSearchListAdapter
 import ch.wyler.microspot.models.Product
 import ch.wyler.microspot.models.SearchApiRequest
 import kotlinx.android.synthetic.main.fragment_search.*
@@ -19,9 +21,10 @@ import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
-class SearchFragment : Fragment(), MatchListAdapter.AdapterCallback {
 
-    private val adapter = MatchListAdapter(this)
+class SearchFragment : Fragment(), ProductSearchListAdapter.AdapterCallback {
+
+    private val adapter = ProductSearchListAdapter(this)
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
@@ -76,6 +79,7 @@ class SearchFragment : Fragment(), MatchListAdapter.AdapterCallback {
      * Callback for the adapter
      */
     override fun onItemClicked(product: Product, position: Int) {
-
+        val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse("http://www.microspot.ch/--P"+ product.code))
+        startActivity(browserIntent)
     }
 }
